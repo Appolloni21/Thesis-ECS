53cf88e5d4370d2fd64ab689484267241293e9da
Appolloni21 <alessandro.appolloni21@gmail.com>
2024-11-27 20:10:00 +0100

    transform - dimensions brand and province

--- data_warehouse_dwh/dags/dbt/dwh_car_fleet/models/dim_brand.sql
+++ data_warehouse_dwh/dags/dbt/dwh_car_fleet/models/dim_brand.sql
@@ -1,12 +1,19 @@
-WITH brand_temp AS (
-    SELECT DISTINCT
+WITH parsed_data AS (
+    SELECT
         dati ->> 'Brand' AS brand,
         dati ->> 'Model' AS model
     FROM {{ source('dwh_car_fleet', 'raw_car_spec') }}
     WHERE dati->>'Brand' IS NOT NULL AND dati->>'Model' IS NOT NULL
+),
+temp_brands AS (
+    SELECT DISTINCT
+        {{ dbt_utils.generate_surrogate_key(['brand']) }} AS brand_id,
+        brand AS brand,
+        model AS model
+    FROM parsed_data
 )
 SELECT
+    brand_id::TEXT,
     brand::TEXT,
-    model:: TEXT
-FROM brand_temp
-
+    model::TEXT
+FROM temp_brands
---
+++ data_warehouse_dwh/dags/dbt/dwh_car_fleet/models/dim_province.sql
@@ -0,0 +1,15 @@
+WITH province_temp AS (
+    SELECT DISTINCT
+        {{ dbt_utils.generate_surrogate_key(['denominazione_provincia']) }} as province_id,
+        denominazione_provincia as province,
+        denominazione_regione as region,
+        ripartizione_geografica as territory
+    FROM {{ source('dwh_car_fleet', 'raw_regions') }}
+    WHERE denominazione_provincia IS NOT NULL
+)
+SELECT
+    province_id::TEXT,
+    province::TEXT,
+    region::TEXT,
+    territory::TEXT
+FROM province_temp
