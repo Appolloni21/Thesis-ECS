781dc72000c46da5528678f0522ee3aabc25f008
Appolloni21 <alessandro.appolloni21@gmail.com>
2024-12-03 19:54:35 +0100

    transformation - some fix on displacement and co2 emissions

--- data_warehouse_dwh/dags/dbt/dwh_car_fleet/models/report_car.sql
+++ data_warehouse_dwh/dags/dbt/dwh_car_fleet/models/report_car.sql
@@ -13,17 +13,26 @@ CASE
     WHEN (dati ->> 'Fuel Type') = 'Diesel / electricity' THEN 'IBRIDO GASOLIO/ELETTRICO'
     WHEN (dati ->> 'Fuel Type') = 'Petrol / LPG' THEN 'B/GPL'
     WHEN (dati ->> 'Fuel Type') = 'LPG' THEN 'GPL'
+    WHEN (dati ->> 'Fuel Type') = 'Hydrogen' THEN 'IDROGENO'
     ELSE NULL
 END AS fuel_type,
 CASE 
      WHEN (dati->>'System power') IS NOT NULL THEN regexp_replace((dati ->> 'System power'), '[^\d].*', '', 'g')::NUMERIC 
-     ELSE regexp_replace((dati ->> 'Power'), '[^\d].*', '', 'g')::NUMERIC 
+     WHEN (dati->>'System power') IS NULL AND (dati->>'Power') IS NULL THEN regexp_replace((dati ->> 'Power (CNG)'), '[^\d].*', '', 'g')::NUMERIC 
+     ELSE regexp_replace((dati->>'Power'), '[^\d].*', '', 'g')::NUMERIC 
 END AS engine_power,
-regexp_replace((dati ->> 'Engine displacement'), '[^\d].*', '', 'g')::DOUBLE PRECISION AS engine_displacement,        
+CASE
+    WHEN (dati ->> 'Fuel Type') = 'Electricity' THEN 0
+    WHEN (dati ->> 'Fuel Type') = 'Hydrogen' THEN 0
+    ELSE regexp_replace((dati ->> 'Engine displacement'), '[^\d].*', '', 'g')::DOUBLE PRECISION
+END AS engine_displacement,        
 regexp_replace((dati ->> 'Kerb Weight'), '[^\d].*', '', 'g')::NUMERIC as kerb_weight,
-CASE 
+CASE
+    WHEN (dati ->> 'Fuel Type') = 'Electricity' THEN 0
+    WHEN (dati ->> 'Fuel Type') = 'Hydrogen' THEN 0
     WHEN (dati ->> 'CO2 emissions') IS NOT NULL THEN regexp_replace((dati ->> 'CO2 emissions'), '[^\d].*', '', 'g')::NUMERIC
     WHEN (dati ->> 'CO2 emissions (WLTC)') IS NOT NULL THEN regexp_replace((dati ->> 'CO2 emissions (WLTC)'), '[^\d].*', '', 'g')::NUMERIC
+    
 END AS CO2_emissions
 FROM {{ source('dwh_car_fleet', 'raw_car_spec') }}
 ),
