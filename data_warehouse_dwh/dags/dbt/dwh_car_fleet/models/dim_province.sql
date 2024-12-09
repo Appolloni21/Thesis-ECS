cb10d0a9d0d0c32f7c0cdfdcba94771c6afb9326
Appolloni21 <alessandro.appolloni21@gmail.com>
2024-12-09 13:16:36 +0100

    dim table province improved

--- data_warehouse_dwh/dags/dbt/dwh_car_fleet/models/dim_province.sql
+++ data_warehouse_dwh/dags/dbt/dwh_car_fleet/models/dim_province.sql
@@ -6,10 +6,30 @@ WITH province_temp AS (
         ripartizione_geografica as territory
     FROM {{ source('dwh_car_fleet', 'raw_province') }}
     WHERE denominazione_provincia IS NOT NULL
+),
+iso_temp AS(
+	SELECT
+		CASE
+			WHEN region_name = 'Forli-Cesena' THEN 'Forlì-Cesena'
+			WHEN region_name = 'Carbonia-Iglesias' THEN 'Sud Sardegna'
+			WHEN region_name = 'Reggio Emilia' THEN 'Reggio Nell''Emilia'
+			WHEN region_name = 'Monza e Brianza' THEN 'Monza e della Brianza'
+			WHEN region_name = 'Bolzano' THEN 'Bolzano/Bozen'
+			WHEN region_name = 'Aosta' THEN 'VALLE D''AOSTA/VALLÉE D''AOSTE'
+			ELSE region_name
+		END AS province,
+		CASE
+			WHEN region_name = 'Carbonia-Iglesias' THEN 'IT-SU'
+            WHEN region_name = 'Pesaro e Urbino' THEN 'IT-PU'
+			ELSE (country_short_code || '-' || regional_code) 
+		END AS iso_code
+	FROM {{ source('dwh_car_fleet', 'raw_iso_code') }}
 )
-SELECT
-    province_id::TEXT,
-    region::TEXT,
-    territory::TEXT
-FROM province_temp
+SELECT DISTINCT ON (pt.province_id)
+	pt.province_id,
+	pt.region,
+	pt.territory,
+	itm.iso_code
+FROM province_temp pt
+INNER JOIN iso_temp itm on pt.province_id = UPPER(itm.province)
 --107 rows
